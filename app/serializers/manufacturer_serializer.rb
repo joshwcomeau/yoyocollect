876b571fc9bcf6c16f7abf9024377a3e994b9515
Joshua Comeau <joshwcomeau@gmail.com>
2014-06-04 13:14:20 -0400

    API actually works! index and show routes do as expected.

--- app/serializers/manufacturer_serializer.rb
+++ app/serializers/manufacturer_serializer.rb
@@ -1,3 +1,3 @@
 class ManufacturerSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :image_url, :created_at, :updated_at
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     resources :manufacturers, only: [:index, :show] do
       resources :yoyos
     end
