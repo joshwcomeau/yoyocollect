66055fd43b1f7baf2701694a685d8b3e58cefbd5
Josh Comeau <joshwcomeau@gmail.com>
2014-06-05 12:59:04 -0400

    Further refactoring of before hooks in rspec tests. Added a third test for selecting individual yoyo info.

--- spec/requests/yoyos_spec.rb
+++ spec/requests/yoyos_spec.rb
@@ -3,12 +3,15 @@ require 'rails_helper'
 describe "Yoyos API" do
 	describe "GET /api/yoyos" do
 		before(:each) do
-			@company = FactoryGirl.create(:manufacturer, name: "YoYoCompany")
+			@company  = FactoryGirl.create(:manufacturer, name: "YoYoCompany")
+			@company2 = FactoryGirl.create(:manufacturer, name: "Company Lodge Yoyo Works")
+			@yoyo1 		= FactoryGirl.create(:yoyo, manufacturers: [@company], model: "Spinning Mayhem", diameter: 55, width:43.25)
+			@yoyo2 		= FactoryGirl.create(:yoyo, manufacturers: [@company], model: "Spinning Mayhem 2")
+			@yoyo3 		= FactoryGirl.create(:yoyo, manufacturers: [@company2], model: "Wooly Elephant")
+
 		end
 
 		it "returns an array of manufacturer names" do
-			FactoryGirl.create(:manufacturer, name: "Company Lodge Yoyo Works")
-
 			get "/api/yoyos", {}, { "Accept" => "application/json" }
 
 			expect(response.status).to eq(200)
@@ -20,11 +23,6 @@ describe "Yoyos API" do
 		end
 
 		it "returns an array of yoyos" do
-			FactoryGirl.create(:yoyo, manufacturers: [@company], model: "Spinning Mayhem")
-			FactoryGirl.create(:yoyo, manufacturers: [@company], model: "Spinning Mayhem 2")
-			@company2 = FactoryGirl.create(:manufacturer, name: "YoYoCompany2")
-			FactoryGirl.create(:yoyo, manufacturers: [@company2], model: "Wooly Elephant")
-
 			get "/api/yoyos", {}, { "Accept" => "application/json" }
 
 			expect(response.status).to eq(200)
@@ -38,6 +36,18 @@ describe "Yoyos API" do
 
 			expect(yoyos).to match_array(["Spinning Mayhem", "Spinning Mayhem 2", "Wooly Elephant"])
 
+		end
+
+		it "returns information about a specific yoyo." do
+			get "/api/yoyos/#{@yoyo1.id}", {}, { "Accept" => "application/json" }
+
+			expect(response.status).to eq(200)
+
+			yoyo = json
+
+			expect(yoyo["model"]).to eq("Spinning Mayhem")
+			expect(yoyo["diameter"]).to eq("55.0")
+			expect(yoyo["width"]).to eq("43.25")
 		end
 
 
