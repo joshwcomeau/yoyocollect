d10e1bc95ac6b43fef5c2e1fd74b1cd63156a640
Joshua Comeau <joshwcomeau@gmail.com>
2014-06-07 14:25:11 -0400

    Added unit tests for user registration (thank you FactoryGirl).

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,10 +2,27 @@ require 'rails_helper'
 
 describe "Users API" do
 	describe "GET /api/yoyos" do
-		before(:each) do
-			@user  = FactoryGirl.create(:user, email: "foo", password: "bar")
+		# before(:each) do
+		# 	@user  = FactoryGirl.create(:user, email: "foo", password: "bar")
 
-		end
+		# end
+
+		it "creates a new user" do
+	    post "/api/users", {
+	    	user: {
+		    	email: 		"test@test.com",
+		    	password: "testtest1",
+		    	password_confirmation: 		"testtest1"
+		    }
+	    }, format: :json
+
+	    # response.should be_success
+	    expect(response.status).to eq(200)
+
+	    user = json
+
+	    expect(User.last["email"]).to eq("test@test.com")
+	  end
 
 		# it "returns an array of manufacturer names" do
 		# 	get "/api/yoyos", {}, { "Accept" => "application/json" }
